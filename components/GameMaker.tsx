4fa2e59b16c569aa3a80979c8b407f4b9e58aa6f
Rob Walker <hi@robwalker.co>
2024-09-23 13:07:47 +0100

    Improve initial screen

--- components/GameMaker.tsx
+++ components/GameMaker.tsx
@@ -16,14 +16,22 @@ export default function GameMaker() {
 
   return (
     <>
-      <Input
-        placeholder="Emojis"
-        type="text"
-        name="emoji"
-        className={"text-2xl font-bold"}
-        value={emoji}
-        onChange={(e) => setEmoji(e.target.value)}
-      />
+      <h1 className="text-2xl font-bold">Create a new Pairs game</h1>
+
+      <fieldset>
+        <label className="text-xl" htmlFor="emoji">
+          What emojis would you like to use?
+        </label>
+
+        <Input
+          placeholder="Emojis"
+          type="text"
+          name="emoji"
+          className={"text-2xl font-bold"}
+          value={emoji}
+          onChange={(e) => setEmoji(e.target.value)}
+        />
+      </fieldset>
 
       <Button type="submit" disabled={!canSubmit}>
         Create Game
